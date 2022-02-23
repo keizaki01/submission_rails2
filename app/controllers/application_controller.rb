c0af7328d158c5d4aba9933eb60c6810c7e635b2
吉崎敬登 <yoshizakikeito@yoshizakitakashitou-no-MacBook-Air.local>
2022-02-23 14:55:06 +0900

    ログイン機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+  # def after_sign_in_path_for(resource)
+  #   reservations_index_path
+  # end
+  def after_sign_out_path_for(resource)
+    reservations_index_path
+  end
+  protect_from_forgery with: :exception
+
+  
+
+  # deviseコントローラーにストロングパラメータを追加する          
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    # サインアップ時にnameのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # アカウント編集の時にnameとprofileのストロングパラメータを追加
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
+
+
+
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  # get 'users/index'
+  # devise_for :users
+  devise_for :users, module: "users"
+  # devise_scope :user do
+  #   get '/users/sign_out' => 'devise/sessions#destroy'
+  # end
+  get 'reservations/index'
+  resources :reservations
+  
+  get "users/show" => "users#show"
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
